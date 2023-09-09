a8e88a1829570d2d0e6dc9f967ee0953358ddc84
Torch-HXM <2468709123@qq.com>
2023-09-09 21:15:23 +0800

    change the beheaver of borning boxes

--- src/main.js
+++ src/main.js
@@ -15,45 +15,84 @@ const scene = new BABYLON.Scene(engine);
 scene.enablePhysics()
 
 // light
-const light = new BABYLON.HemisphericLight( "light1", new BABYLON.Vector3(0, 1, 0), scene);
+const light = new BABYLON.HemisphericLight( "light", new BABYLON.Vector3(0, 1, 0), scene);
 light.intensity = 0.7;
 
 // camera
 var camera = new BABYLON.ArcRotateCamera("camera", 45, 0, 0, new BABYLON.Vector3(0, 0, -0), scene);
-camera.setPosition(new BABYLON.Vector3(0, 130, -130));
+camera.setPosition(new BABYLON.Vector3(0, 150, -150));
 camera.attachControl(canvas, true);
 
-// ground
-const ground = new BABYLON.MeshBuilder.CreateGround("ground", {width: 300, height: 300}, scene);
-ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, {mass:0, restitution:0.5}, scene);
-// container's edges
-var container_size = 72;
-var container_material = new BABYLON.StandardMaterial("container material", scene);
-container_material.alpha = 0.3;
+// visiable edges
+var container_base_data = {
+  "size":72,
+  "depth":0.1,
+  "material":new BABYLON.StandardMaterial("container edges material", scene), 
+  "alpha":0.3,
+  "restitution":0.5,
+}
+container_base_data["material"].alpha = container_base_data["alpha"];
+var container_loop_data = {
+  "name":["left edge", "right edge", "front edge", "back edge", "bottom edge"],
+  "size":[[container_base_data["depth"], container_base_data["size"]/2, container_base_data["size"]],
+          [container_base_data["depth"], container_base_data["size"]/2, container_base_data["size"]],
+          [container_base_data["size"], container_base_data["size"]/2, container_base_data["depth"]],
+          [container_base_data["size"], container_base_data["size"]/2, container_base_data["depth"]],
+          [container_base_data["size"], container_base_data["depth"], container_base_data["size"]]],
+  "position":[new BABYLON.Vector3(-(container_base_data["size"] + container_base_data["depth"])/2, (container_base_data["size"]/4 + container_base_data["depth"]/2), 0), 
+              new BABYLON.Vector3((container_base_data["size"] + container_base_data["depth"])/2, (container_base_data["size"]/4 + container_base_data["depth"]/2), 0),
+              new BABYLON.Vector3(0, (container_base_data["size"]/4 + container_base_data["depth"]/2), -(container_base_data["size"] + container_base_data["depth"])/2),
+              new BABYLON.Vector3(0, (container_base_data["size"]/4 + container_base_data["depth"]/2), (container_base_data["size"] + container_base_data["depth"])/2),
+              new BABYLON.Vector3(0, 0, 0)]
+};
+for(var i=0;i<container_loop_data["name"].length;i++){
+  var edge = new BABYLON.MeshBuilder.CreateBox(container_loop_data["name"][i], 
+                                              {width:container_loop_data["size"][i][0], height:container_loop_data["size"][i][1], depth:container_loop_data["size"][i][2]}, 
+                                              scene);
+  edge.material = container_base_data["material"];
+  edge.physicsImpostor = new BABYLON.PhysicsImpostor(edge, BABYLON.PhysicsImpostor.BoxImpostor, {mass:0, restitution:container_base_data["restitution"]}, scene);
+  edge.position = container_loop_data["position"][i];
+}
 
-var edges = [
-  new BABYLON.MeshBuilder.CreateBox( "edge left", {width:0.1, height:container_size/2, depth:container_size}, scene ),
-  new BABYLON.MeshBuilder.CreateBox( "edge right", {width:0.1, height:container_size/2, depth:container_size}, scene),
-  new BABYLON.MeshBuilder.CreateBox( "edge front", {width:container_size, height:container_size/2, depth:0.1}, scene),
-  new BABYLON.MeshBuilder.CreateBox( "edge back", {width:container_size, height:container_size/2, depth:0.1}, scene ),
-];
-edges[0].position = new BABYLON.Vector3(-container_size/2, container_size/4, 0);
-edges[1].position = new BABYLON.Vector3(container_size/2, container_size/4, 0);
-edges[2].position = new BABYLON.Vector3(0, container_size/4, -container_size/2);
-edges[3].position = new BABYLON.Vector3(0, container_size/4, container_size/2);
-
-for(var i=0; i<4; i++){
-  edges[i].physicsImpostor = new BABYLON.PhysicsImpostor(edges[i], BABYLON.PhysicsImpostor.BoxImpostor, {mass:0, restitution:0.5}, scene);
-  edges[i].material = container_material;
+// invisiable edges
+var limit_base_data = {
+  "size":72,
+  "depth":0.1,
+  "material":new BABYLON.StandardMaterial("limit edges material", scene), 
+  "alpha":0,
+  "restitution":0.5,
+}
+limit_base_data["material"].alpha = limit_base_data["alpha"];
+var limit_loop_data = {
+  "name":["left edge", "right edge", "front edge", "back edge", "top edge", "bottom edge"],
+  "size":[[limit_base_data["depth"], limit_base_data["size"], limit_base_data["size"]],
+          [limit_base_data["depth"], limit_base_data["size"], limit_base_data["size"]],
+          [limit_base_data["size"], limit_base_data["size"], limit_base_data["depth"]],
+          [limit_base_data["size"], limit_base_data["size"], limit_base_data["depth"]],
+          [limit_base_data["size"], limit_base_data["depth"], limit_base_data["size"]],
+          [limit_base_data["size"], limit_base_data["depth"], limit_base_data["size"]]],
+  "position":[new BABYLON.Vector3(-(limit_base_data["size"] + limit_base_data["depth"])/2, (limit_base_data["size"] + limit_base_data["depth"])/2, 0), 
+              new BABYLON.Vector3((limit_base_data["size"] + limit_base_data["depth"])/2, (limit_base_data["size"] + limit_base_data["depth"])/2, 0),
+              new BABYLON.Vector3(0, (limit_base_data["size"] + limit_base_data["depth"])/2, -(limit_base_data["size"] + limit_base_data["depth"])/2),
+              new BABYLON.Vector3(0, (limit_base_data["size"] + limit_base_data["depth"])/2, (limit_base_data["size"] + limit_base_data["depth"])/2),
+              new BABYLON.Vector3(0, (limit_base_data["size"] + limit_base_data["depth"]), 0),
+              new BABYLON.Vector3(0, 0, 0)]
+};
+for(var i=0;i<limit_loop_data["name"].length;i++){
+  var edge = new BABYLON.MeshBuilder.CreateBox(limit_loop_data["name"][i], 
+                                              {width:limit_loop_data["size"][i][0], height:limit_loop_data["size"][i][1], depth:limit_loop_data["size"][i][2]}, 
+                                              scene);
+  edge.material = limit_base_data["material"];
+  edge.physicsImpostor = new BABYLON.PhysicsImpostor(edge, BABYLON.PhysicsImpostor.BoxImpostor, {mass:0, restitution:limit_base_data["restitution"]}, scene);
+  edge.position = limit_loop_data["position"][i];
 }
 
 // boxes
 var counter = 0;
-var unit_normal_vector = new BABYLON.Vector3(0, 1, 0)
-var boxes_property = {"box":[], "matrix":[]};
+var boxes_data = {"box":[], "matrix":[]};
 
 function createRandomBox(){
-  if(counter < 30){
+  if(counter < 200){
     var random_num = Math.floor(Math.random()*3);
     var mat = new BABYLON.StandardMaterial("map1", scene);
     var options = getTexture(
@@ -69,7 +108,7 @@ function createRandomBox(){
 
     var box = new BABYLON.MeshBuilder.CreateBox("medicine"+counter, options, scene);
     box.material = mat;
-    box.position = new BABYLON.Vector3(Math.random()*container_size/2-container_size/4, container_size, Math.random()*container_size/2-container_size/4);
+    box.position = new BABYLON.Vector3(Math.random()*container_base_data["size"]/2-container_base_data["size"]/4, container_base_data["size"], Math.random()*container_base_data["size"]/2-container_base_data["size"]/4);
     box.rotation = new BABYLON.Vector3(Math.random()*Math.PI, Math.random()*Math.PI, Math.random()*Math.PI);
     box.physicsImpostor = new BABYLON.PhysicsImpostor(
       box, 
@@ -78,10 +117,10 @@ function createRandomBox(){
       scene
     );
     box.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(0, -20, 0));
-    boxes_property["box"].push(box);
+    boxes_data["box"].push(box);
     counter += 1;
 
-    setTimeout(createRandomBox, 200);
+    setTimeout(createRandomBox, 20);
   }
 }
 createRandomBox();
